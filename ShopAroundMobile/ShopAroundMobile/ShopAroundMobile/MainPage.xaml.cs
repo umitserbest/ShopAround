1677aed9cd51b57bf07d4fa9dec72945be798eb9
Ümit Serbest <16966763+umitserbest@users.noreply.github.com>
2019-03-02 14:14:19 +0300

    Xamarin Project Files

---
+++ ShopAroundMobile/ShopAroundMobile/ShopAroundMobile/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ShopAroundMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
